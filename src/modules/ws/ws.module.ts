b1514027708ae6145653979057c1b20aa0a40418
Thiago Morgado <thiago_morgado@hotmail.com>
2026-07-17 00:52:45 -0300

    fix: forwardRef no WsModule para resolver dependencia circular com RidesModule

--- src/modules/ws/ws.module.ts
+++ src/modules/ws/ws.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { WsAppGateway } from './ws-passenger.gateway';
@@ -9,7 +9,7 @@ import { SessionKickModule } from '../../shared/session-kick/session-kick.module
 
 @Module({
   imports: [
-    RidesModule,
+    forwardRef(() => RidesModule),
     DriversModule,
     AuthModule,
     SessionKickModule,
